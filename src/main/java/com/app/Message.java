b7acde35422409d0ab98ffad63344294827e9709
Krzysztof <programowanie.krzysiek@gmail.com>
2019-11-20 19:51:16 +0100

    km-1

--- src/main/java/com/app/Message.java
+++ src/main/java/com/app/Message.java
@@ -8,4 +8,20 @@ public class Message {
         this.title = title;
         this.content = content;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
