d5f7421aa3b31ae3a6bd7dca7e66512961a09b98
pawel <pawel@gmail.com>
2019-11-20 19:36:17 +0100

    v4

--- src/main/java/com/app/Person.java
+++ src/main/java/com/app/Person.java
@@ -3,9 +3,4 @@ package com.app;
 public class Person {
     private String name;
     private Integer age;
-
-    public Person(String name, Integer age) {
-        this.name = name;
-        this.age = age;
-    }
 }
