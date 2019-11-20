3eebe2e2d6483374aaa9f1388f814daa954cd252
pawel <pawel@gmail.com>
2019-11-20 19:32:04 +0100

    v2

--- src/main/java/com/app/Person.java
+++ src/main/java/com/app/Person.java
@@ -3,4 +3,9 @@ package com.app;
 public class Person {
     private String name;
     private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
 }
